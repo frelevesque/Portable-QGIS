7390efc31496246e412627ef1d2dfa3f60ed5c5a
frelevesque <frederic.lev@gmail.com>
2020-03-04 17:43:03 -0500

    Create qgis-ltr.bat

---
+++ QGIS_3.x.x_LTR/qgis-ltr.bat
@@ -0,0 +1,14 @@
+@ECHO OFF
+REM This batch file make QGIS 3.x.x portable on windows
+REM Config will be stored in QGIS 3.x.x/profiles/
+REM The batch file calls a Python script that correct the path each time QGIS is started via the batch file
+
+SET BATCH_PATH=%~dp0
+
+SET QGIS_ROOT=%BATCH_PATH%qgis
+
+"%QGIS_ROOT%\apps\Python37\pythonw.exe" %BATCH_PATH%portable\qgis-ltr.pyw
+
+timeout /t 3
+
+taskkill /f /im cmd.exe >nul
