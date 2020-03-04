fb42bbf725787a41251076ae10e4333d18511461
frelevesque <frederic.lev@gmail.com>
2020-03-04 17:38:55 -0500

    Update qgis-grass7.bat

--- QGIS_3.x.x/qgis-grass7.bat
+++ QGIS_3.x.x/qgis-grass7.bat
@@ -5,7 +5,7 @@ REM The batch file calls a Python script that correct the path each time QGIS is
 
 SET BATCH_PATH=%~dp0
 
-SET QGIS_ROOT=BATCH_PATH%qgis
+SET QGIS_ROOT=%BATCH_PATH%qgis
 
 "%QGIS_ROOT%\apps\Python37\pythonw.exe" %BATCH_PATH%portable\qgis-grass7.pyw
 
